f96fe5cb9c62438cbc89a4779b67aac70b811d72
AnishRM-ai <np03cs4a220045@heraldcollege.edu.np>
2024-03-30 11:51:19 +0545

    Added files to git repo.

---
+++ cart/cart.py
@@ -0,0 +1,66 @@
+from store.models import Product
+import datetime
+
+class Cart():
+    def __init__(self, request):
+        self.session = request.session
+        
+        # Get the current session key if it exists
+        cart = self.session.get('session_key')
+        
+        #if the user is new, no session key, Create one!
+        if 'session_key' not in request.session:
+            cart = self.session['session_key'] = {}
+            
+        # Make sure cart is available on all pages of sites
+        self.cart = cart
+        
+    def add(self, product,quantity):
+      
+        product_id = str(product.id)
+        product_qty = str(quantity)
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            # self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
+        self.session.modified = True
+        
+    def __len__(self):
+        return len(self.cart)
+    
+    def get_prods(self):
+        #Get ids from cart
+        product_ids = self.cart.keys()
+        # Use ids to lookup products in database model
+        products = Product.objects.filter(id__in=product_ids)
+        #Return those lookup products
+        return products
+    
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
+    
+    def update(self, product, quantity):
+        product_id = str(product)
+        product_qty = int(quantity)
+    
+        # get cart
+        ourcart = self.cart
+        #update dictionary/cart
+        ourcart[product_id] = product_qty 
+        
+        self.session.modified = True
+        thing = self.cart
+        return thing
+    
+    def remove(self, product):
+        product_id = str(product)
+        #delete from dict/cart
+        if product_id in self.cart:
+            del self.cart[product_id]
+            
+        self.session.modified = True
+        
+    
