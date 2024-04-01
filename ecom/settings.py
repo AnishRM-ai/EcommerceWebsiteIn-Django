21a03d4c49a165acb7a97bcfef6c6d88fdd12452
AnishRM-ai <np03cs4a220045@heraldcollege.edu.np>
2024-04-01 11:40:42 +0545

    POSTGRESQL configured

--- ecom/settings.py
+++ ecom/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'Luga',
+        'USER':'postgres',
+        'PASSWORD':'1234',
+        'HOST':'localhost',
+        'PORT': '5432',
     }
 }
 
